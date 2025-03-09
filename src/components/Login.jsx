8299e52cdf615279ecb8c158f8357a7b12c6f63e
Argel Alcantara <argeldan28@gmail.com>
2025-03-09 22:21:04 +0100

    third commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,13 +7,16 @@ export default function Login(){
 
                 <div className='login-form'>
                     <h1>Login Form</h1>
+                    
+                    <div className="input-group">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" id='username' name='username' placeholder='Username' required/>
+                    </div>
 
-                    <label htmlFor="username">Username</label>
-                    <input type="text" id='username' name='username' placeholder='Username' required/>
-
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id='password' name='password' placeholder='Password' required/>
-
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id='password' name='password' placeholder='Password' required/>
+                    </div>
                     <label>
                         <input type='checkbox' />Remember me
                     </label>
