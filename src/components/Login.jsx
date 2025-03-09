93123f348e5a84ab4f968edf284323a9c5d484a6
Argel Alcantara <argeldan28@gmail.com>
2025-03-09 22:02:48 +0100

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,33 @@
+import './Login.css'
+
+export default function Login(){
+    return(
+        <>
+            <div className='container'>
+
+                <div className='login-form'>
+                    <h1>Login Form</h1>
+
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id='username' name='username' placeholder='Username' required/>
+
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id='password' name='password' placeholder='Password' required/>
+
+                    <label>
+                        <input type='checkbox' />Remember me
+                    </label>
+
+                    <button className='btn-login'>Login</button>
+                </div>
+                
+                
+                <div className='welcome'>
+                    <h1>Welcome back!</h1>
+                    <p>Please enter your username and password to access your account. Don't forget to check 'Remember me' if you want to stay logged in. If you don’t have an account yet, sign up now to get started!</p>
+                    <button className='btn-register'>Register</button>
+                </div>
+            </div>
+        </>
+    )
+}
